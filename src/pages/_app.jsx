f7c0ea72b03783455840a011788603d72bccd161
Katherine Davenia <katherinedavenia24@gmail.com>
2022-10-05 23:43:35 +0700

    [TD] setting up environment

---
+++ src/pages/_app.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
